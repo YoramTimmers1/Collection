5c334a17b42e12f95529b9c5046efbead5ca6a61
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-12-15 12:03:02 +0100

    listen

---
+++ listen 2.py
@@ -0,0 +1,24 @@
+listone = [1,2,3,4,5,6,7,8,9,10]
+listtwo = [2,4,6,8,10,12,14,16,18,20]
+
+
+def listenoptellen(listone,listtwo):
+    for i in range(len(listone)):
+        print(listone[i] + listtwo[i])
+listenoptellen(listone,listtwo)
+
+def listenoptellen(listone,listtwo):
+    for i in range(len(listone)):
+        print(listone[i] - listtwo[i])
+listenoptellen(listone,listtwo)
+
+def listenoptellen(listone,listtwo):
+    for i in range(len(listone)):
+        print(listone[i] * listtwo[i])
+listenoptellen(listone,listtwo)
+
+def listenoptellen(listone,listtwo):
+    for i in range(len(listone)):
+        print(listone[i] / listtwo[i])
+listenoptellen(listone,listtwo)
+
---
+++ listen 3.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+def spelprogamma():
+    import random
+    for i in range(3):
+        spellist = ["Yathzee", "Bridge", "Poker", "Pesten", "Mens erger je niet","Cluedo"]
+        print(random.choice(spellist))
+    
+spelprogamma()
+
+print("------------------------------------------------------------------------------------")
+
+def spelprogamma():
+    import random
+    for i in range(10):
+        spellist = ["Yathzee", "Bridge", "Poker", "Pesten", "Mens erger je niet","Cluedo"]
+        print(random.choice(spellist))
+    
+spelprogamma()
