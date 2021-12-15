517bc3fd18fbdd77a9e3dfaa6ea032cc4b795499
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-12-15 09:09:00 +0100

    listen'

---
+++ Listen.py
@@ -0,0 +1,9 @@
+alledagen = ["ma" , "di", "wo","do","vr","za","zo"]
+
+print(alledagen[0:7])
+print(alledagen[0:5])
+print(alledagen[6:7])
+alledagen.reverse
+print(alledagen[0:7])
+print(alledagen[0:5])
+print(alledagen[5:7]) 
